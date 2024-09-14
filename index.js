45a15a14d7b8d4bb34b92dc5c18ffb7cdcfe10bc
Medalknight <36373645+Medalknight@users.noreply.github.com>
2024-09-14 10:22:58 +0200

    Update index.js
    Update via ChatGPT für Kompatibilität mit Homebridge V2

--- index.js
+++ index.js
@@ -1,143 +1,144 @@
 "use strict";
 
-var Service, Characteristic, HomebridgeAPI;
-const { HomebridgeDummyVersion } = require('./package.json');
+// Service und Characteristic werden von der homebridge-API bereitgestellt
+let Service, Characteristic;
+const { HomebridgeDummyVersion } = require('./package.json'); // Version aus package.json
+const storage = require('node-persist'); // Storage-Modul für persistente Speicherung von Zuständen
 
-module.exports = function(homebridge) {
-
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  HomebridgeAPI = homebridge;
-  homebridge.registerAccessory("homebridge-dummy", "DummySwitch", DummySwitch);
+module.exports = (api) => {
+  // Registrierung des Zubehörs
+  api.registerAccessory("homebridge-dummy", "DummySwitch", DummySwitch);
 }
 
-
-function DummySwitch(log, config) {
+function DummySwitch(log, config, api) { // Der Constructor nimmt jetzt auch die api entgegen
   this.log = log;
   this.name = config.name;
   this.stateful = config.stateful;
   this.dimmer = config.dimmer;
-  this.brightness = config.brightness;
+  this.brightness = config.brightness || 0;
   this.brightnessStorageKey = this.name + "Brightness";
   this.reverse = config.reverse;
-  this.time = config.time ? config.time : 1000;		
+  this.time = config.time || 1000; // Standardwert gesetzt, falls nicht konfiguriert
   this.resettable = config.resettable;
-  this.timer = null;
   this.random = config.random;
   this.disableLogging = config.disableLogging;
 
+  // API und Cache-Verzeichnis
+  this.api = api; // Homebridge-API-Instanz speichern
+  this.cacheDirectory = this.api.user.persistPath(); // Pfad zur Persistenz
+
+  // Initialisierung des Speichers
+  storage.initSync({ dir: this.cacheDirectory, forgiveParseErrors: true });
+
+  // Festlegen des Service-Typs: Dimmer oder Switch
   if (this.dimmer) {
-	this._service = new Service.Lightbulb(this.name);
-	this.modelString = "Dummy Dimmer";
+    this._service = new Service.Lightbulb(this.name);
+    this.modelString = "Dummy Dimmer";
   } else {
-	this._service = new Service.Switch(this.name);
-	this.modelString = "Dummy Switch";
+    this._service = new Service.Switch(this.name);
+    this.modelString = "Dummy Switch";
   }
-  
+
+  // Accessory Information Service
   this.informationService = new Service.AccessoryInformation();
   this.informationService
-      .setCharacteristic(Characteristic.Manufacturer, 'Homebridge')
-      .setCharacteristic(Characteristic.Model, this.modelString)
-      .setCharacteristic(Characteristic.FirmwareRevision, HomebridgeDummyVersion)
-      .setCharacteristic(Characteristic.SerialNumber, 'Dummy-' + this.name.replace(/\s/g, '-'));
-  
-  this.cacheDirectory = HomebridgeAPI.user.persistPath();
-  this.storage = require('node-persist');
-  this.storage.initSync({dir:this.cacheDirectory, forgiveParseErrors: true});
-  
+    .setCharacteristic(Characteristic.Manufacturer, 'Homebridge')
+    .setCharacteristic(Characteristic.Model, this.modelString)
+    .setCharacteristic(Characteristic.FirmwareRevision, HomebridgeDummyVersion)
+    .setCharacteristic(Characteristic.SerialNumber, 'Dummy-' + this.name.replace(/\s/g, '-'));
+
+  // Schalter On/Off
   this._service.getCharacteristic(Characteristic.On)
     .on('set', this._setOn.bind(this));
+
+  // Dimmer: Helligkeit hinzufügen
   if (this.dimmer) {
-	this._service.getCharacteristic(Characteristic.Brightness)
-            .on('get', this._getBrightness.bind(this))
-            .on('set', this._setBrightness.bind(this));
+    this._service.getCharacteristic(Characteristic.Brightness)
+      .on('get', this._getBrightness.bind(this))
+      .on('set', this._setBrightness.bind(this));
   }
 
-  if (this.reverse) this._service.setCharacteristic(Characteristic.On, true);
+  // Initialisierung des Schalterzustands
+  if (this.reverse) {
+    this._service.setCharacteristic(Characteristic.On, true);
+  }
 
+  // Stateful Switch: Zustand aus Storage laden
   if (this.stateful) {
-	var cachedState = this.storage.getItemSync(this.name);
-	if((cachedState === undefined) || (cachedState === false)) {
-		this._service.setCharacteristic(Characteristic.On, false);
-	} else {
-		this._service.setCharacteristic(Characteristic.On, true);
-	}
+    const cachedState = storage.getItemSync(this.name);
+    this._service.setCharacteristic(Characteristic.On, cachedState === undefined ? false : cachedState);
   }
 
+  // Dimmer: Helligkeit aus Storage laden
   if (this.dimmer) {
-	var cachedBrightness = this.storage.getItemSync(this.brightnessStorageKey);
-	if ((cachedBrightness == undefined) || cachedBrightness == 0) {
-		this._service.setCharacteristic(Characteristic.On, false);
-		this._service.setCharacteristic(Characteristic.Brightness, 0);
-	} else {
-		this._service.setCharacteristic(Characteristic.On, true);
-		this._service.setCharacteristic(Characteristic.Brightness, cachedBrightness);
-	}
+    const cachedBrightness = storage.getItemSync(this.brightnessStorageKey);
+    this._service.setCharacteristic(Characteristic.Brightness, cachedBrightness === undefined ? 0 : cachedBrightness);
+    this._service.setCharacteristic(Characteristic.On, cachedBrightness > 0);
   }
 }
 
-DummySwitch.prototype.getServices = function() {
+DummySwitch.prototype.getServices = function () {
+  // Rückgabe der verfügbaren Dienste
   return [this.informationService, this._service];
 }
 
-function randomize(time) {
-  return Math.floor(Math.random() * (time + 1));
-}
-
-DummySwitch.prototype._getBrightness = function(callback) {
-
-  if ( ! this.disableLogging ) {
-	this.log("Getting " + "brightness: " + this.brightness);
+// Helligkeit abrufen (Dimmer-Modus)
+DummySwitch.prototype._getBrightness = function (callback) {
+  if (!this.disableLogging) {
+    this.log("Getting brightness: " + this.brightness);
   }
-
-  callback(null, this.brightness);
+  callback(null, this.brightness); // Rückgabe des aktuellen Helligkeitswerts
 }
 
-DummySwitch.prototype._setBrightness = function(brightness, callback) {
-
-  if ( ! this.disableLogging ) {
-	var msg = "Setting brightness: " + brightness
-	this.log(msg);
+// Helligkeit setzen (Dimmer-Modus)
+DummySwitch.prototype._setBrightness = function (brightness, callback) {
+  if (!this.disableLogging) {
+    this.log("Setting brightness: " + brightness);
   }
 
   this.brightness = brightness;
-  this.storage.setItemSync(this.brightnessStorageKey, brightness);
+  storage.setItemSync(this.brightnessStorageKey, brightness); // Helligkeitswert speichern
 
   callback();
 }
 
-DummySwitch.prototype._setOn = function(on, callback) {
+// Schalterzustand setzen
+DummySwitch.prototype._setOn = function (on, callback) {
+  const delay = this.random ? randomize(this.time) : this.time;
+  const msg = `Setting switch to ${on}`;
 
-  var delay = this.random ? randomize(this.time) : this.time;
-  var msg = "Setting switch to " + on
   if (this.random && !this.stateful) {
-      if (on && !this.reverse || !on && this.reverse) {
-        msg = msg + " (random delay " + delay + "ms)"
-      }
-  }
-  if( ! this.disableLogging ) {
-      this.log(msg);
+    this.log(msg + ` (random delay ${delay}ms)`);
+  } else if (!this.disableLogging) {
+    this.log(msg);
   }
 
+  // Automatisches Zurücksetzen des Schalters nach Ablauf der Zeit
   if (on && !this.reverse && !this.stateful) {
     if (this.resettable) {
       clearTimeout(this.timer);
     }
-    this.timer = setTimeout(function() {
+    this.timer = setTimeout(() => {
       this._service.setCharacteristic(Characteristic.On, false);
-    }.bind(this), delay);
+    }, delay);
   } else if (!on && this.reverse && !this.stateful) {
     if (this.resettable) {
       clearTimeout(this.timer);
     }
-    this.timer = setTimeout(function() {
+    this.timer = setTimeout(() => {
       this._service.setCharacteristic(Characteristic.On, true);
-    }.bind(this), delay);
+    }, delay);
   }
-  
+
+  // Stateful-Schalterzustand speichern
   if (this.stateful) {
-	this.storage.setItemSync(this.name, on);
+    storage.setItemSync(this.name, on);
   }
-  
+
   callback();
+}
+
+// Zufälligen Verzögerungswert generieren
+function randomize(time) {
+  return Math.floor(Math.random() * (time + 1));
 }
